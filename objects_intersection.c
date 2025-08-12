b8c6def382af50f9c58be42f4060ca1b3e759881
gm-infinite <kuzeyberk18@gmail.com>
2025-08-12 21:57:56 +0300

    Calculates intersection points for objects
    Implements functions to determine the intersection points between rays and planes, and rays and spheres.
    
    These functions calculate the 't' value along the ray where the intersection occurs, returning -1 if no intersection is found or the 't' value is negative.
    This change replaces the previous implementation that returned intersection points directly.

--- objects_intersection.c
+++ objects_intersection.c
@@ -6,52 +6,46 @@
 /*   By: kuzyilma <kuzyilma@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 22:11:11 by kuzyilma          #+#    #+#             */
-/*   Updated: 2025/08/11 22:56:17 by kuzyilma         ###   ########.fr       */
+/*   Updated: 2025/08/12 21:57:34 by kuzyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "objects.h"
 #include <math.h>
 
-t_intersection_point	plane_intersection(t_ray ray, t_plane plane)
+double	plane_intersection(t_ray ray, t_plane plane)
 {
-	t_intersection_point	ret;
-	double					d;
+    double	d;
+    double	t;
 
-	d = vector_dot_product(plane.direction, ray.direction);
-	if (fabs(d) < T_ZERO_THRESHOLD)
-		return (intersection_point_default());
-	ray.t = vector_dot_product(point_substract(plane.origin, ray.origin),
-			plane.direction) / d;
-	if (ray.t < 0)
-		return (intersection_point_default());
-	ret.intersection_point = vector_add(ray.origin,
-			vector_constant_multipy(ray.t, ray.direction));
-	ret.surface_normal = plane.direction;
-	return (ret);
+    d = vector_dot_product(plane.direction, ray.direction);
+    if (fabs(d) < T_ZERO_THRESHOLD)
+        return (-1);
+    t = vector_dot_product(point_substract(plane.origin, ray.origin),
+            plane.direction) / d;
+    if (t < T_ZERO_THRESHOLD)
+        return (-1);
+    return (t);
 }
 
-t_intersection_point	sphere_intersection(t_ray ray, t_sphere sphere)
+double	sphere_intersection(t_ray ray, t_sphere sphere)
 {
-	t_intersection_point	ret;
+	double					t1;
+	double					t2;
 	t_vector				oc;
-	double					a;
 	double					b;
 	double					discriminant;
 
 	oc = point_substract(ray.origin, sphere.origin);
-	a = vector_dot_product(ray.direction, ray.direction);
 	b = 2.0 * vector_dot_product(oc, ray.direction);
-	discriminant = (b * b) - (4.0 * a * (vector_dot_product(oc, oc)
-				- (sphere.radius * sphere.radius)));
+	discriminant = (b * b) - (4.0 * (vector_dot_product(oc, oc) - (sphere.radius * sphere.radius)));
 	if (discriminant < 0)
-		return (intersection_point_default());
-	ray.t = (-b - sqrt(discriminant)) / (2.0 * a);
-	if (ray.t < 0)
-		return (intersection_point_default());
-	ret.intersection_point = vector_add(ray.origin,
-			vector_constant_multipy(ray.t, ray.direction));
-	ret.surface_normal = vector_normilize(point_substract(
-				ret.intersection_point, sphere.origin));
-	return (ret);
+		return (-1);
+	t1 = (-b - sqrt(discriminant)) / 2.0;
+	t2 = (-b + sqrt(discriminant)) / 2.0;
+	if (t1 >= T_ZERO_THRESHOLD)
+		return (t1);
+	else if (t2 >= T_ZERO_THRESHOLD)
+		return (t2);
+	return (-1);
 }
