d8dea90dd900644b3535a44f51da77770fa25de8
gm-infinite <kuzeyberk18@gmail.com>
2025-08-12 22:00:09 +0300

    norm fixes
    fixed norm errors

--- objects_intersection.c
+++ objects_intersection.c
@@ -6,7 +6,7 @@
 /*   By: kuzyilma <kuzyilma@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 22:11:11 by kuzyilma          #+#    #+#             */
-/*   Updated: 2025/08/12 21:57:34 by kuzyilma         ###   ########.fr       */
+/*   Updated: 2025/08/12 21:59:48 by kuzyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 double	plane_intersection(t_ray ray, t_plane plane)
 {
-    double	d;
-    double	t;
+	double	d;
+	double	t;
 
-    d = vector_dot_product(plane.direction, ray.direction);
-    if (fabs(d) < T_ZERO_THRESHOLD)
-        return (-1);
-    t = vector_dot_product(point_substract(plane.origin, ray.origin),
-            plane.direction) / d;
-    if (t < T_ZERO_THRESHOLD)
-        return (-1);
-    return (t);
+	d = vector_dot_product(plane.direction, ray.direction);
+	if (fabs(d) < T_ZERO_THRESHOLD)
+		return (-1);
+	t = vector_dot_product(point_substract(plane.origin, ray.origin),
+			plane.direction) / d;
+	if (t < T_ZERO_THRESHOLD)
+		return (-1);
+	return (t);
 }
 
 double	sphere_intersection(t_ray ray, t_sphere sphere)
@@ -38,7 +38,8 @@ double	sphere_intersection(t_ray ray, t_sphere sphere)
 
 	oc = point_substract(ray.origin, sphere.origin);
 	b = 2.0 * vector_dot_product(oc, ray.direction);
-	discriminant = (b * b) - (4.0 * (vector_dot_product(oc, oc) - (sphere.radius * sphere.radius)));
+	discriminant = (b * b) - (4.0 * (vector_dot_product(oc, oc)
+				- (sphere.radius * sphere.radius)));
 	if (discriminant < 0)
 		return (-1);
 	t1 = (-b - sqrt(discriminant)) / 2.0;
